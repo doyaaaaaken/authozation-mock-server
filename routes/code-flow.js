3af22119a047245ff92ef62e9d487d5aedc0c5b3
doyaaaaaken <kk03140816@gmail.com>
2018-02-06 22:52:11 +0900

    Fix api bug and documentation mistakes with Running dredd test.

--- routes/code-flow.js
+++ routes/code-flow.js
@@ -3,33 +3,33 @@ const router = require('express').Router();
 const preDefinedScopeList = ["openid", "profile", "email", "address", "phone"];
 
 //TODO: Write UT
-const validate = (scopeList, responseType, clientId, redirectUrl, state) => {
+const validate = (scopeList, responseType, clientId, redirectUri, state) => {
     if(!scopeList) {
         return { status: 400, msg: "Bad request: 'scope' parameter is REQUIRED." };
     } else if (!scopeList.includes('openid')) {
         return { status: 400, msg: "scope parameter list must contains 'openid' value." };
     }
-    if(responseType) {
+    if(!responseType) {
         return { status: 400, msg: "Bad request: 'response_type' parameter is REQUIRED." };
-    } else if (responseType === 'code') {
+    } else if (responseType !== 'code') {
         //TODO: Support Implicit flow
         return { status: 400, msg: "'scope' parameter must have value 'code' in authorization code flow." };
     }
-    if(clientId) {
+    if(!clientId) {
         return { status: 400, msg: "Bad request: 'client_id' parameter is REQUIRED." };
     }
-    if(redirectUrl) {
-        return { status: 400, msg: "Bad request: 'redirect_url' parameter is REQUIRED." };
+    if(!redirectUri) {
+        return { status: 400, msg: "Bad request: 'redirect_uri' parameter is REQUIRED." };
     }
 
-    return { status: 200, msg: `Receive request params.
+    return { status: 302, msg: `Receive request params.
                 scope = ${scopeList}
                 response_type = ${responseType}
                 client_id = ${clientId}
-                redirect_url = ${redirectUrl}
+                redirect_uri = ${redirectUri}
                 state = ${state}`};
 };
-const parseScopeList = (scope) => scope.split(' ').filter((value) => {
+const parseScopeList = (scope) => (scope ? scope : "").split(' ').filter((value) => {
     if(preDefinedScopeList.includes(value)) {
         return true;
     } else {
@@ -38,9 +38,9 @@ const parseScopeList = (scope) => scope.split(' ').filter((value) => {
     }
 });
 
-const createResponseData = (res, scope, responseType, clientId, redirectUrl, state) => {
+const createResponseData = (res, scope, responseType, clientId, redirectUri, state) => {
     const scopeList = parseScopeList(scope);
-    const responseParams = validate(scopeList, responseType, clientId, redirectUrl, state);
+    const responseParams = validate(scopeList, responseType, clientId, redirectUri, state);
     console.log(responseParams.msg);
     //TODO: change response to officially format. (http://openid-foundation-japan.github.io/openid-connect-core-1_0.ja.html#AuthResponse)
     res.status(responseParams.status).send(responseParams.msg);
@@ -51,19 +51,19 @@ router
         const scope = req.query["scope"]; //ex.) "openid%20profile"
         const responseType = req.query["response_type"]; //ex.) "code"
         const clientId = req.query["client_id"]; //ex.) "s6BhdRkqt3"
-        const redirectUrl = req.query["redirect_url"]; //ex.) "https%3A%2F%2Fclient.example.org%2Fcb"
+        const redirectUri = req.query["redirect_uri"]; //ex.) "https%3A%2F%2Fclient.example.org%2Fcb"
         const state = req.query["state"]; //ex.) "af0ifjsldkj"
 
-        createResponseData(res, scope, responseType, clientId, redirectUrl, state);
+        createResponseData(res, scope, responseType, clientId, redirectUri, state);
     })
     .post('/authorize', (req, res, next) => {
         const scope = req.body.scope;
         const responseType = req.body.response_type;
         const clientId = req.body.client_id;
-        const redirectUrl = req.body.redirect_url;
+        const redirectUri = req.body.redirect_uri;
         const state = req.body.state;
 
-        createResponseData(res, scope, responseType, clientId, redirectUrl, state);
+        createResponseData(res, scope, responseType, clientId, redirectUri, state);
     })
     .post('/token', (req, res, next) => {
         //TODO: implement (http://openid-foundation-japan.github.io/openid-connect-core-1_0.ja.html#TokenEndpoint)
