5ab53285a34c764d8e461f0267b6f3df47fc1e53
doyaaaaaken <kk03140816@gmail.com>
2018-02-11 00:12:59 +0900

    Implement prototype of Resource Owner Password Credentials Flow token endpoint.

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -1,84 +1,68 @@
 const router = require('express').Router();
 
-//TODO: Write UT
-const validate = (scopeList, responseType, clientId, redirectUri, state) => {
-    if(!scopeList) {
-        return { status: 400, msg: "Bad request: 'scope' parameter is REQUIRED." };
-    } else if (!scopeList.includes('openid')) {
-        return { status: 400, msg: "scope parameter list must contains 'openid' value." };
-    }
-    if(!responseType) {
-        return { status: 400, msg: "Bad request: 'response_type' parameter is REQUIRED." };
-    } else if (responseType !== 'code') {
-        //TODO: Support Implicit flow
-        return { status: 400, msg: "'scope' parameter must have value 'code' in authorization code flow." };
-    }
-    if(!clientId) {
-        return { status: 400, msg: "Bad request: 'client_id' parameter is REQUIRED." };
-    }
-    if(!redirectUri) {
-        return { status: 400, msg: "Bad request: 'redirect_uri' parameter is REQUIRED." };
-    }
-
-    return { status: 302, msg: `Receive request params.
-                scope = ${scopeList}
-                response_type = ${responseType}
-                client_id = ${clientId}
-                redirect_uri = ${redirectUri}
-                state = ${state}`};
-};
-
 const parseScopeList = (scope) => (scope ? scope : "").split(' ');
-
-const createResponseData = (res, scope, responseType, clientId, redirectUri, state) => {
-    const scopeList = parseScopeList(scope);
-    const responseParams = validate(scopeList, responseType, clientId, redirectUri, state);
-    console.log(responseParams.msg);
-    //TODO: change response to officially format. (http://openid-foundation-japan.github.io/openid-connect-core-1_0.ja.html#AuthResponse)
-    res.status(responseParams.status).send(responseParams.msg);
-};
-
-
 
 class TokenApiSuccessResponse {
 
-    constructor(access_token, token_type, expires_in) {
+    constructor(access_token, token_type, expires_in, refresh_token) {
         this.access_token = access_token;
         this.token_type = token_type;
         this.expires_in = expires_in;
+        this.refresh_token = refresh_token;
+    }
+
+    static buildForResourceOwnerPasswordCredentialsFlow(access_token, token_type, expires_in, refresh_token) {
+        return new TokenApiSuccessResponse(access_token, token_type, Number(expires_in), refresh_token);
     }
 
     static buildForClientCredentialFlow(access_token, token_type, expires_in) {
-        return new TokenApiSuccessResponse(access_token, token_type, Number(expires_in));
+        return new TokenApiSuccessResponse(access_token, token_type, Number(expires_in), null);
     }
 }
 
+const EXPIRES_IN = 3600;
 //TODO: generate access token from uuid.
 
-
 //TODO: implement flow 1.
-//TODO: implement flow 2.
-//TODO: implement flow 3.
 router
     .get('/authorize', (req, res, next) => {
+        //TODO: implement all flows
         const scope = req.query["scope"]; //ex.) "openid%20profile"
         const responseType = req.query["response_type"]; //ex.) "code"
         const clientId = req.query["client_id"]; //ex.) "s6BhdRkqt3"
         const redirectUri = req.query["redirect_uri"]; //ex.) "https%3A%2F%2Fclient.example.org%2Fcb"
         const state = req.query["state"]; //ex.) "af0ifjsldkj"
 
-        createResponseData(res, scope, responseType, clientId, redirectUri, state);
+        // createResponseData(res, scope, responseType, clientId, redirectUri, state);
     })
     .post('/token', (req, res, next) => {
         const grantType = req.body.grant_type;
         const scopeList = parseScopeList(req.body.scope);
 
-        //(Client Credential Grant Flow)[https://tools.ietf.org/html/rfc6749#section-4.4]
-        if(grantType === 'client_credentials') {
-            const responseBody = TokenApiSuccessResponse.buildForClientCredentialFlow('xxx-acc-token', 'bearer', 3600);
+        if (grantType === 'authorization_code') { //Authorization Code Flow [https://tools.ietf.org/html/rfc6749#section-4.1]
+
+        } else if (grantType === 'password') { //Resource Owner Password Credentials Flow [https://tools.ietf.org/html/rfc6749#section-4.3]
+            const username = req.body.username;
+            const password = req.body.password;
+
+            //TODO: implement error resopnse
+            if(!username) {
+                res.status(400).send("parameter username required.");
+            } else if (!password) {
+                res.status(400).send("parameter username required.");
+            } else if (username === 'test' && password === 'testtest') {
+                const responseBody = TokenApiSuccessResponse.buildForResourceOwnerPasswordCredentialsFlow('xxxxxx', 'bearer', EXPIRES_IN, 'xxxxx');
+                res.json(responseBody);
+            } else {
+                res.status(401).send('');
+            }
+
+        } else if (grantType === 'client_credentials') { //Client Credential Grant Flow [https://tools.ietf.org/html/rfc6749#section-4.4]
+            const responseBody = TokenApiSuccessResponse.buildForClientCredentialFlow('xxx-acc-token', 'bearer', EXPIRES_IN);
             res.json(responseBody);
+
         } else {
-            res.status(400).send("grant_type parameter must be 'authorization_code'.");
+            res.status(400).send("grant_type parameter wrong.");
         }
     });
 
