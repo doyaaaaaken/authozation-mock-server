e5b1466f86c35408c7e15e12f9c2cf0a1b59e4f8
doyaaaaaken <kk03140816@gmail.com>
2018-02-10 23:58:03 +0900

    Implement Client Credentials Flow.

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-const preDefinedScopeList = ["openid", "profile", "email", "address", "phone"];
-
 //TODO: Write UT
 const validate = (scopeList, responseType, clientId, redirectUri, state) => {
     if(!scopeList) {
@@ -29,14 +27,8 @@ const validate = (scopeList, responseType, clientId, redirectUri, state) => {
                 redirect_uri = ${redirectUri}
                 state = ${state}`};
 };
-const parseScopeList = (scope) => (scope ? scope : "").split(' ').filter((value) => {
-    if(preDefinedScopeList.includes(value)) {
-        return true;
-    } else {
-        console.warn(`scope value '${value}' is ignored!! Because it's not defined value.`);
-        return false;
-    }
-});
+
+const parseScopeList = (scope) => (scope ? scope : "").split(' ');
 
 const createResponseData = (res, scope, responseType, clientId, redirectUri, state) => {
     const scopeList = parseScopeList(scope);
@@ -46,6 +38,27 @@ const createResponseData = (res, scope, responseType, clientId, redirectUri, sta
     res.status(responseParams.status).send(responseParams.msg);
 };
 
+
+
+class TokenApiSuccessResponse {
+
+    constructor(access_token, token_type, expires_in) {
+        this.access_token = access_token;
+        this.token_type = token_type;
+        this.expires_in = expires_in;
+    }
+
+    static buildForClientCredentialFlow(access_token, token_type, expires_in) {
+        return new TokenApiSuccessResponse(access_token, token_type, Number(expires_in));
+    }
+}
+
+//TODO: generate access token from uuid.
+
+
+//TODO: implement flow 1.
+//TODO: implement flow 2.
+//TODO: implement flow 3.
 router
     .get('/authorize', (req, res, next) => {
         const scope = req.query["scope"]; //ex.) "openid%20profile"
@@ -57,13 +70,17 @@ router
         createResponseData(res, scope, responseType, clientId, redirectUri, state);
     })
     .post('/token', (req, res, next) => {
-        //TODO: implement (http://openid-foundation-japan.github.io/openid-connect-core-1_0.ja.html#TokenEndpoint)
         const grantType = req.body.grant_type;
-        if(grantType !== 'authorization_code') {
-            res.status(400).send("grant_type parameter must be 'authorization_code'.");
+        const scopeList = parseScopeList(req.body.scope);
+
+        //(Client Credential Grant Flow)[https://tools.ietf.org/html/rfc6749#section-4.4]
+        if(grantType === 'client_credentials') {
+            const responseBody = TokenApiSuccessResponse.buildForClientCredentialFlow('xxx-acc-token', 'bearer', 3600);
+            res.json(responseBody);
         } else {
-            res.send('');
+            res.status(400).send("grant_type parameter must be 'authorization_code'.");
         }
     });
+
 
 module.exports = router;
