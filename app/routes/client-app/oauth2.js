edb2d9ba495ed0ed46b7bee1a82def7d1f81b01e
doyaaaaaken <kk03140816@gmail.com>
2018-02-12 00:03:21 +0900

    Implement oauth2.0 callback endpoint.

--- app/routes/client-app/oauth2.js
+++ app/routes/client-app/oauth2.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
+const request = require('request');
 
 router
     .get('/', (req, res, next) => {
-        res.render('oauth2', { title: 'OAuth2' });
+        res.render('oauth2', {title: 'OAuth2 Sample'});
     })
+    //This endpoint is called by Authorization Code Flow.
     .get('/callback', (req, res, next) => {
-        //TODO: implement
+        const code = req.query["code"];
+        const state = req.query["state"];
+
+        const options = {
+            uri: 'http://localhost:3000/auth-server/oauth2/token',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            form: {
+                "grant_type": "authorization_code",
+                "code": (code) ? code : null,
+                "redirect_uri": "http://localhost:3000/client-app/oauth2/callback",
+                "client_id": "cid-xxx-12345"
+            }
+        };
+        request.post(options, (error, response, body) => {
+            if(error) {
+                res.render('oauth2-result', {result: 'ERROR'});
+            } else {
+                if(response.statusCode !== 200) {
+                    res.render('oauth2-result', {result: 'Failed', description: JSON.stringify(response.body)});
+                } else {
+                    res.render('oauth2-result', {result: 'Success', description: JSON.stringify(response.body)});
+                }
+            }
+        });
     });
 
 module.exports = router;
